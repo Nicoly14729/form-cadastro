9f493c812506a46b6fa458e240c3a4f163fede5a
Nicoly <nicolythayna901@gmail.com>
2023-05-10 09:44:04 -0300

    Finaliza projeto de recuperação

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # form-cadastro
-Formulário de cadastro para simular o cadastro de sites de e-commerce como Americanas e Magazine Luiza.
+Formulário de pesquisa.
--- main.js
+++ main.js
@@ -1,35 +1,13 @@
 function Verificar() {
 
-        let CEP = document.getElementById('loginCep').value;
+     let name = document.getElementById('loginName').value;
     
-        let endereço = document.getElementById('loginEndereço').value;
+     let email = document.getElementById('loginEmail').value;
     
-        let numero = document.getElementById('loginNúmero').value;
+     let idade = document.getElementById('loginIdade').value;
     
-        let complemento = document.getElementById('loginComplemento').value;
+     if (!name || !email || !idade)
     
-        let bairro = document.getElementById('loginBairro').value;
-    
-        let cidade = document.getElementById('loginCidade').value;
-    
-        let estado = document.getElementById('loginEstado').value;
-    
-        let referencia = document.getElementById('loginpontodereferência').value;
-    
-        let email = document.getElementById('loginEmail').value;
-    
-        let password = document.getElementById('loginSenha').value;
-    
-        let telefone = document.getElementById('loginTelefone').value;
-    
-        let cpf = document.getElementById('loginCpf').value;
-    
-        let datanasc = document.getElementById('loginDatadenascimento').value;
-    
-        let name = document.getElementById('loginName').value;
-    
-        if (!email || !password || !CEP || !endereço || !numero || !complemento || !bairro || !cidade || !estado || !referencia || !telefone || !cpf || !datanasc || !name)
-    
-        alert("Campos de preenchimento obrigatório. Favor preencher")
+     alert("Campos de preenchimento obrigatório. Favor preencher")
     
     }
